b274b95ea92e1dbea0e1d77de50fcf2e5ca5297d
mauroaicode <mauricioaicode@gmail.com>
2025-10-31 22:51:58 -0500

    Se realiza ajustes

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -4,5 +4,10 @@ return [
     'invalid_or_expired_code' => 'The code is invalid or has expired.',
     'exists' => 'The selected :attribute is invalid.',
     'same' => 'The :attribute field must match :other.',
+    'numeric' => 'The :attribute field must be a number.',
+    'min' => [
+        'numeric' => 'The :attribute field must be at least :min.',
+        'string' => 'The :attribute field must be at least :min characters.',
+    ],
 ];
 
